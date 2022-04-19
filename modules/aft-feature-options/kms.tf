3642836a1139418d597e48032e3ea67d00b82f09
Ahmed <khmeda@f4d488823db3.ant.amazon.com>
2022-04-19 12:47:46 -0500

    First Commit

---
+++ modules/aft-feature-options/kms.tf
@@ -0,0 +1,18 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+resource "aws_kms_key" "aft_log_key" {
+  provider            = aws.log_archive
+  description         = "KMS key for encrypt/decrypt log files"
+  enable_key_rotation = "true"
+  policy = templatefile("${path.module}/kms/key-policies/log-key.tpl", {
+    log_archive_account_id = var.log_archive_account_id
+  })
+}
+
+resource "aws_kms_alias" "aft_log_key_alias" {
+  provider = aws.log_archive
+
+  name          = "alias/aft"
+  target_key_id = aws_kms_key.aft_log_key.key_id
+}
---
+++ sources/aft-lambda-layer/aft_common/types.py
@@ -0,0 +1,18 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+from typing import Literal, TypedDict
+
+
+class AftAccountInfo(TypedDict):
+    id: str
+    email: str
+    name: str
+    joined_method: str
+    joined_date: str
+    status: str
+    parent_id: str
+    parent_type: str
+    org_name: str
+    type: Literal["account"]
+    vendor: Literal["aws"]
